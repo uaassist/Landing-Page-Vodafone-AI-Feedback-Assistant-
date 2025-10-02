32364e8af43aaf75aa3ed11144e5bc372aa1856a
uaassist <s.tenkov@liketo.me>
2025-10-01 20:32:51 -0400

    Update concierge.js

--- functions/concierge.js
+++ functions/concierge.js
@@ -48,7 +48,7 @@ exports.handler = async function (event) {
       method: 'POST',
       headers: { 
         'Content-Type': 'application/json', 
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`, // This line was causing the crash
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
         model: 'gpt-4-turbo',
